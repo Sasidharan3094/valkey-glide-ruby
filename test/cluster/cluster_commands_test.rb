2e8c5f7e84516538bd62cb13e747243e87a3872a
Sasidharan Gopal <sasidharan.gopal94@gmail.com>
2025-12-01 14:14:53 +0530

    Adding module & pubsub to cluster tests
    Signed-off-by: Sasidharan Gopal <sasidharan.gopal94@gmail.com>

--- test/cluster/cluster_commands_test.rb
+++ test/cluster/cluster_commands_test.rb
@@ -6,5 +6,7 @@ class TestClusterCommandsOnClusters < Minitest::Test
   include Helper::Cluster
   # include Lint::StringCommands # Run string tests first (while cluster is healthy)
   include Lint::ConnectionCommands # Run connection tests (cluster-aware)
+  include Lint::PubSubCommands # Pub/Sub works in cluster mode
+  include Lint::ModuleCommands # Module commands work in cluster mode (per-node)
   include Lint::ClusterCommands # Run cluster commands second (after string tests)
 end
